abc0130f0e435eba2c0fc2a11a7c1746a86d315f
kinshuk1207 <93797250+kinshuk1207@users.noreply.github.com>
2023-03-27 17:20:24 -0400

    Implemented Vision transfomer

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import tensorflow_addons as tfa
 
 from PIL import Image
 from sklearn.model_selection import train_test_split
@@ -45,6 +48,100 @@ def split_data(train_df: pd.DataFrame,
 
     return x_train, x_test, y_train, y_test
 
+# Some helper functions for ViT
+
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super().__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super().__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+
+def create_vit_classifier():
+    num_classes = 15
+    image_size = 200  # We'll resize input images to this size
+    input_shape = (200, 200, 3)
+    patch_size = 10  
+    num_patches = (image_size // patch_size) ** 2
+    projection_dim = 64
+    num_heads = 4
+    transformer_units = [
+        projection_dim * 2,
+        projection_dim,
+    ]  # Size of the transformer layers
+    transformer_layers = 8
+    mlp_head_units = [512, 15]  # Size of the dense layers of the final classifier
+
+    inputs = layers.Input(shape=input_shape)
+    # Augment data.
+    # augmented = data_augmentation(inputs)
+    augmented = inputs # No data augmentation for now
+    # Create patches.
+    patches = Patches(patch_size)(augmented)
+    # Encode patches.
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        # Layer normalization 1.
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x3, x2])
+
+    # Create a [batch_size, projection_dim] tensor.
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    representation = layers.Flatten()(representation)
+    representation = layers.Dropout(0.5)(representation)
+    # Add MLP.
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    # Classify outputs.
+    logits = layers.Dense(num_classes, activation='softmax')(features)
+    # Create the Keras model.
+    model = keras.Model(inputs=inputs, outputs=logits)
+    return model
+
+
 # Build the model for predicting the action
 def train_model(model_config: str, 
                 x_train: np.ndarray, 
@@ -80,6 +177,7 @@ def train_model(model_config: str,
             - EfficientNetV2M
             - ConvNeXtLarge
             - CNN
+            - ViT
 
         Returns:
             a tuple containing the following objects:
@@ -137,6 +235,9 @@ def train_model(model_config: str,
                     tf.keras.layers. Dropout(0.2),
                     tf.keras.layers. Dense(15, activation='softmax')
         ])
+        
+    elif model_config == 'ViT':
+        model = create_vit_classifier()
 
         model.compile(
             optimizer=tf.keras.optimizers.Adam(),
